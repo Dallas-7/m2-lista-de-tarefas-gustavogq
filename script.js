33e4b9ce023357782ed18d941aca6928aa9f9428
Dallas <gustavogq777@gmail.com>
2025-05-08 20:20:13 -0300

    js parcialmente feito

--- script.js
+++ script.js
@@ -10,3 +10,76 @@ const tasks = [
   {title: "Pagar a conta de energia", type: "Urgente"},
   {title: "Assistir a um documentário interessante", type: "Normal"},
 ];
+
+function renderElements(tasks){
+  const listaTarefa = document.querySelector(".tasks__list")
+
+  listaTarefa.innerHTML = ""
+
+  for (let i = 0; i < tasks.length; i++){
+    const itemLista = createTaskItem(tasks[i]);
+    listaTarefa.appendChild(itemLista)
+  }
+}
+
+function createTaskItem(tarefa) {
+  const li = document.createElement("li")
+  const div = document.createElement("div")
+  const span = document.createElement("span")
+  const p = document.createElement("p")
+  const button = document.createElement("button")
+
+  li.classList.add("task__item")
+  div.classList.add("task-info__container")
+  span.classList.add("task-type")
+  button.classList.add("task__button--remove-task")
+
+  if (tarefa.type.toLowerCase() === "urgente") {
+    span.classList.add("span-urgent");
+  } else if (tarefa.type.toLowerCase() === "importante") {
+    span.classList.add("span-important");
+  } else if (tarefa.type.toLowerCase() === "normal") {
+    span.classList.add("span-normal");
+  }
+
+  p.textContent = tarefa.title
+
+  div.appendChild(span);
+  div.appendChild(p);
+  li.appendChild(div);
+  li.appendChild(button);
+
+  return li;
+}
+
+const botao = document.querySelector(".form__button--add-task")
+const form = document.querySelector('.form__container');
+const titleInput = document.getElementById('input_title');
+const typeSelect = document.querySelector('.form__input--priority');
+
+botao.addEventListener("click", function(event){
+  event.preventDefault();
+
+  const titleValue = titleInput.value.trim()
+  const typeValue = typeSelect.value.trim()
+
+  if (!titleValue || !typeValue){
+    alert('por favor, preencha todos os campos');
+    return;
+  }
+
+  const novaTarefa = {
+    title: titleValue,
+    type: typeValue.charAt(0).toUpperCase() + typeValue.slice(1)
+  }
+
+  tasks.push(novaTarefa);
+  renderElements(tasks);
+
+  titleInput.value = '';
+  typeSelect.value = '';
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+  renderElements(tasks);
+});
